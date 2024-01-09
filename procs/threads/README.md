e99ed0534afbc90cdea686768e86d2f8f5513370
Adeel Ahmad <6880680+adeelahmadk@users.noreply.github.com>
2024-01-10 01:14:58 +0500

    add new programs, refactor readme docs
    Added new programs and refactored the readme for the 'threads' section.

--- procs/threads/README.md
+++ procs/threads/README.md
@@ -23,4 +23,17 @@ This type uniquely identifies a thread.
 > IEEE Std 1003.1-2001/Cor 2-2004, item XBD/TC2/D6/26 is applied, adding pthread_t to the list of types that are not required to be arithmetic types, thus allowing pthread_t to be defined as a structure.
 > [Stackoverflow Answer](https://stackoverflow.com/a/33285994/5892622)
 
+## Example Programs
 
+| Program | Description |
+| ------- | ----------- |
+| `threads.c` | A simple example of starting threads using `pthread` library. |
+| `thread_args.c` | An example on sending arguments to threads. |
+| `thread_ret.c` | An example of receiving return value from a thread. |
+| `thread_join.c` | How to wait for the threads to finish. |
+| `mutex.c` | Memory read/write safety through Mutex Locks. |
+| `count_unex.c` | Different threads updating same memory unexculsively. |
+| `count_mutex.c` | Different threads updating same memory in a mutually exculsive (Mutex) manner. |
+|  |  |
+|  |  |
+|  |  |
--- procs/threads/threadio.c
+++ procs/threads/threadio.c
@@ -8,44 +8,58 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
-// #include <signal.h>
 #include <fcntl.h>
-#include <stdarg.h>         // variadic function
-#include <errno.h>
-// #include <sys/time.h>
+#include <pthread.h>
 
 #define SERVER_PORT 80
 
 #define MAXLINE 4096
 #define SA struct sockaddr
 
-void err_n_die(const char *fmt, ...);
+#define BIG 1000000000UL
+uint32_t counter = 0;
 
-int main(int argc, char *argv[])
-{
+void * count_to_big(void *arg);
+char *getHomePage(char *addr, int *bytesread);
+
+int main() {
+    pthread_t t;
+    char *buf;
+    int buflen;
+
+    pthread_create(&t, NULL, count_to_big, NULL);
+    buf = getHomePage("172.217.0.78", &buflen);
+    pthread_join(t, NULL);
+
+    printf("Done. Counter: %u, Received %d bytes\n", counter, buflen);
+    printf("%s\n", buf);
+
+    return 0;
+}
+
+char *getHomePage(char *addr, int *bytesread) {
     int sockfd, n;
     int sendbytes;
     struct sockaddr_in servaddr;
     char sendline[MAXLINE];
-    char recvline[MAXLINE];
-
-    if(argc != 2) {
-        err_n_die("usage: %s <server address>\n", argv[0]);
-    }
+    char *buffer = malloc(MAXLINE);
 
     if( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 )
-        err_n_die("Error while creating the socket!");
+        return NULL;
+        // err_n_die("Error while creating the socket!");
 
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(SERVER_PORT); // host to network, short
 
     // string to binary representation for IP address
-    if(inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
-        err_n_die("inet_pton error for %s ", argv[1]);
+    if(inet_pton(AF_INET, addr, &servaddr.sin_addr) <= 0)
+        return NULL;
+        // err_n_die("inet_pton error for %s ", addr);
 
     if(connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0)
-        err_n_die("connect failed!");
+        return NULL;
+        // err_n_die("connect failed!");
 
     // prepare the message
     //          request  path  protocol+version
@@ -58,37 +72,24 @@ int main(int argc, char *argv[])
     // some of the bytes are sent.
     // normally, you would want to retry, unless the return value was -1
     if(write(sockfd, sendline, sendbytes) != sendbytes)
-        err_n_die("write error!");
+        return NULL;
+        // err_n_die("write error!");
 
-    memset(recvline, 0, MAXLINE);
+    memset(buffer, 0, MAXLINE);
     // read server's response
-    while ( (n = read(sockfd, recvline, MAXLINE-1)) > 0 ) {
-        printf("%s", recvline);
-    }
+    n = read(sockfd, buffer, MAXLINE-1);
     close(sockfd);
     if(n < 0)
-        err_n_die("read error!");
+        return NULL;
 
-    printf("\n** Reading Completed! **\n");
-    exit(0); // exit successfully
+    *bytesread = n;
+    return buffer;
 }
 
-void err_n_die(const char *fmt, ...) {
-    int errno_save;
-    va_list ap;
-
-    errno_save = errno;
-
-    va_start(ap, fmt);
-    vfprintf(stdout, fmt, ap);
-    fprintf(stdout, "\n");
-    fflush(stdout);
-
-    if(errno_save != 0) {
-        fprintf(stdout, "(errno = %d) : %s\n", errno_save, strerror(errno_save));
-        fprintf(stdout, "\n");
-        fflush(stdout);
+void * count_to_big(void *arg) {
+    for(uint32_t i = 0; i < BIG; i++) {
+        counter++;
     }
-    va_end(ap);
-    exit(1);
+
+    return NULL;
 }
